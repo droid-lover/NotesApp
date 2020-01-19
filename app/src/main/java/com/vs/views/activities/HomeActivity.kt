526a3857cea88801c2a7a9d7ee997fdeee0c0328
myJarvis <ersachin20@gmail.com>
2020-01-19 15:13:25 +0530

    Added Splash Screen with lottie and flow to HomeActivity

--- app/src/main/java/com/vs/views/activities/HomeActivity.kt
+++ app/src/main/java/com/vs/views/activities/HomeActivity.kt
@@ -1,14 +1,15 @@
-package com.vs
+package com.vs.views.activities
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.vs.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
